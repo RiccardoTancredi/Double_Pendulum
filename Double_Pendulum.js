72d0d4879d6b7ff8db41913388009db2c7fa1fdb
RiccardoTancredi <ric.tancredi@gmail.com>
2020-09-15 20:59:57 +0200

    HotFix
    Now the js pendulum script can show the motion of the pendulum.
    Fixed the same in the double pendulum

--- Double_Pendulum.js
+++ Double_Pendulum.js
@@ -1,25 +1,14 @@
 let resolution = 50;
 const origin_1 = [500,50];
-// let phase_1 =  0.0;
-// let phase_2 =  0.0;
+
 let a1 = 0.0;
 let a2 = 0.0;
 let a1_v = 0;
 let a2_v = 0;
 let lenght_1 = 250;
 let lenght_2 = 250;
-// let angle_1 = amplitude_1;
-// let angle_2 = amplitude_2;
-let g = 1;
-// let T_1 = 2*Math.PI*Math.sqrt(lenght_1/g);
-// let T_2 = 2*Math.PI*Math.sqrt(lenght_2/g);
-// let arch_1 = 2*lenght_1*a1;
-// let t_1 = 0.00;
-// let t_2 = 0.00;
-// let omega_1 = Math.sqrt(g/lenght_1);
-// let omega_2 = Math.sqrt(g/lenght_2);
-// console.log("La pulsazione del primo pendolo vale: ", omega_1);
-// console.log("Il periodo di Oscillazione del primo pendolo vale:", T_1);
+
+let g = 1; //With 9.8 it too fast
 
 let buffer;
 let px2 = -1;
@@ -39,21 +28,16 @@ function setup(){
 
 function mouseClicked(){
     background(255);
-    //buffer.stroke(255);
-    //buffer.background(255);
+
+    frameCount = 0;
+
     clear();
     buffer.clear();
     buffer.background(255);
     let initial_coordinates_1 = [mouseX,mouseY];
     a1 = Math.atan(abs((initial_coordinates_1[0]-origin_1[0]))/(initial_coordinates_1[1]-origin_1[1]));
     lenght_1 = Math.sqrt((initial_coordinates_1[0]-origin_1[0])**2 + (initial_coordinates_1[1]-origin_1[1])**2);
-    // arch_1 = 2*lenght_1*a1;
-    // T_1 = 2*Math.PI*Math.sqrt(lenght_1/g);
-    // omega_1 = Math.sqrt(g/lenght_1);
-    // t_1 = 0.0;
 
-    // console.log("La pulsazione del primo pendolo vale: ", omega_1);
-    // console.log("Il periodo di Oscillazione del primo pendolo vale:", T_1);
 }
 
 
@@ -104,30 +88,12 @@ function draw(){
     a2_v += a2_a;
     a1 += a1_v;
     a2 += a2_v;
-    
-    // Drawing the First Pendulum
-    // line(origin_1[0],origin_1[1], lenght_1*sin(phase_1+angle_1) + origin_1[0], lenght_1*cos(phase_1+angle_1)+origin_1[1]);
-    // circle(lenght_1*sin(phase_1+angle_1) + origin_1[0], lenght_1*cos(phase_1+angle_1) + origin_1[1], resolution);
-    // angle_1 = amplitude_1*cos(omega_1*t_1+phase_1);
-    // //t += 0.020; //This in order to make the pendulum looks more realistic, in terms of oscillation time, without friction
-    // //otherwise
-    // //t_1 += T_1/arch_1;
-    // t_1 += 1;
-    // //console.log(t_1);
-    // // Drawing the Second Pendulum
-    // origin_2 = [lenght_1*sin(phase_1+angle_1) + origin_1[0], lenght_1*cos(phase_1+angle_1) + origin_1[1]];
-    // line(origin_2[0],origin_2[1], lenght_2*sin(phase_2+angle_2) + origin_2[0], lenght_2*cos(phase_2+angle_2)+origin_2[1]);
-    // circle(lenght_2*sin(phase_2+angle_2) + origin_2[0], lenght_2*cos(phase_2+angle_2) + origin_2[1], resolution);
-    // //line(lenght_2*sin(phase_2+angle_1) + origin_2[0], lenght_2*cos(phase_2+angle_1) + origin_2[1], lenght_2*sin(phase_2+angle_2) + origin_2[0], lenght_2*cos(phase_2+angle_2) + origin_2[1]);
-    // angle_2 = cos(omega_2*t_2+phase_2) + angle_1;
-    // t_2 = t_1;
 
     buffer.stroke(0);
-    if (frameCount > 1) {
+    if (frameCount > 25) {
         buffer.line(px2, py2, x2, y2);
     }
 
     px2 = x2;
     py2 = y2;
 }
-
--- pendulum.js
+++ pendulum.js
@@ -1,5 +1,5 @@
 let resolution = 50;
-const origin = [300,50];
+const origin = [500,50];
 let phase =  0.0;
 let amplitude = Math.PI/4;
 let lenght = 250;
@@ -8,21 +8,31 @@ let g = 9.81;
 let T = 2*Math.PI*Math.sqrt(lenght/g)*(1+(amplitude**2)/16);
 let arch = 2*lenght*amplitude;
 let t = 0.00;
-//Trying to draw the arch. Not so good, and no continuous drawing, Just the entire arch printed at the start
-// const a = origin[0];
-// const b = origin[1] + lenght*Math.cos(amplitude);
-// const w = 2*lenght*Math.sin(amplitude);
-// const h = lenght;
+
+let buffer;
+let px2 = 0.0;
+let py2 = 0.0;
+
 let omega = Math.sqrt(g/lenght);
 console.log("La pulsazione vale: ", omega);
 console.log("Il periodo di Oscillazione vale:", T);
 
 function setup(){
-    createCanvas(600, 600);
+    createCanvas(1000, 1000);
     stroke(255);  
+    pixelDensity(1);
+    buffer = createGraphics(width, height);
+    buffer.background(255);
+    buffer.translate(origin[0], origin[1]);
 }
 
 function mouseClicked(){
+    background(255);
+    frameCount = 0;
+    clear();
+    buffer.clear();
+    buffer.background(255);
+
     let initial_coordinates = [mouseX,mouseY];
     amplitude = Math.atan(abs((initial_coordinates[0]-origin[0]))/(initial_coordinates[1]-origin[1]));
     lenght = Math.sqrt((initial_coordinates[0]-origin[0])**2 + (initial_coordinates[1]-origin[1])**2);
@@ -30,29 +40,42 @@ function mouseClicked(){
     T = 2*Math.PI*Math.sqrt(lenght/g)*(1+(amplitude**2)/16);
     omega = Math.sqrt(g/lenght);
     t = 0.0;
-    // const a = origin[0];
-    // const b = origin[1] + lenght*Math.cos(amplitude);
-    // const w = 2*lenght*Math.sin(amplitude);
-    // const h = lenght;
+
     console.log("La pulsazione vale: ", omega);
     console.log("Il periodo di Oscillazione vale:", T);
 }
 
 
 function draw(){
+    //Display of the pendulum oscillation 
     background(0);
+    imageMode(CORNER);
+    image(buffer, 0, 0, width, height);
+
+    translate(origin[0], origin[1]);
+    stroke(0);
+    strokeWeight(2);
+
     // Ceiling
-    line(0,50,600,50); 
+    line(-origin[0],0,1000-origin[0],0); 
+
     // Drawing the Pendulum
-    line(origin[0],origin[1], lenght*sin(phase+angle) + origin[0], lenght*cos(phase+angle)+origin[1]);
-    circle(lenght*sin(phase+angle) + origin[0], lenght*cos(phase+angle) + origin[1], resolution);
-    //noFill();
-    //arc(a, b, w, h, Math.PI/2 - amplitude,  Math.PI/2 + amplitude);
-    //fill(255);
+    let x2 = lenght*sin(phase+angle);
+    let y2 = lenght*cos(phase+angle);
+    line(0,0, x2, y2);
+    circle(x2, y2, resolution);
+
     angle = amplitude*cos(omega*t+phase);
-    t += 0.020; //This in order to make the pendulum looks more realistic, in terms of oscillation time, without friction
+    //t += 0.020; //This in order to make the pendulum looks more realistic, in terms of oscillation time, without friction
     //otherwise
-    //t += T/arch;
+    t += T/arch;
     //console.log(t);
+    buffer.stroke(0);
+    if (frameCount > 25) {
+        buffer.line(px2, py2, x2, y2);
+    }
+
+    px2 = x2;
+    py2 = y2;
 }
 
